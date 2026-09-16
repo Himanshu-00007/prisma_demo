ee78c71101f5148367e55febf0beba0ca97aee60
Himanshu Pandey <himanshupandey2003@gmail.com>
2026-09-16 19:46:39 +0530

    implementing some crud operations

--- backend/index.ts
+++ backend/index.ts
@@ -7,10 +7,24 @@ const port=8000;
 
 
 app.get("/users",async (req,res)=>{
-    const users=await prisma.user.findFirst();
-    res.send(users);
+    const users=await prisma.user.findMany({
+        where:{
+            AND:[{id:{gt:4}},{name:"Himanshu"}]
+        }
+    });
+    res.json(users);
 })
 
+
+app.put("/update",async(req,res)=>{
+    const updatedUser=await prisma.user.update({
+        where:{email:"something@gmail.com"},
+        data:{
+            name:"something"
+        }
+    })
+    res.json(updatedUser);
+})
 
 app.listen(port,()=>{
     console.log("server is listening");
